645481e53be25cb9fcfc49e1f1a30c106e8a11b7
Samuel Lando <samuel.lando@aol.com>
2023-03-29 12:25:58 -0400

    first commit

---
+++ the-librarians-nook/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import BookList from './BookList.svelte';
+</script>
+
+<div id="page">
+	<h1>The Librarian's Nook</h1>
+	<BookList />
+</div>
+
+<style>
+	h1 {
+		color: #cc00ff;
+		font-size: 6vh;
+		text-align: center;
+		font-family: Luminari, fantasy;
+	}
+</style>
---
+++ the-librarians-nook/src/routes/BookList.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	let books = [
+		{
+			image:
+				'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1170803558l/72193.jpg',
+			title: 'Harry Potter and the Philosopher’s Stone',
+			goodReads:
+				'https://www.goodreads.com/book/show/72193.Harry_Potter_and_the_Philosopher_s_Stone?from_search=true&from_srp=true&qid=vHgcnNSuGC&rank=1',
+			ownership: {
+				sam: 'Free on kindle with amazon prime',
+				merry: 'Owns it'
+			},
+			status: 'Suggested',
+			notes: {
+				merry: 'this is a test',
+				sam: 'this is a test 2'
+			}
+		},
+		{
+			image:
+				'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1574199214i/52767659.jpg',
+			title: 'The End of Everything',
+			goodReads:
+				'https://www.goodreads.com/book/show/52767659-the-end-of-everything?ref=nav_sb_ss_1_21',
+			ownership: {
+				sam: 'Will buy on kindle',
+				merry: "Can borrow Fabio's copy"
+			},
+			status: 'Suggested',
+			notes: {
+				merry: 'this is a test',
+				sam: 'this is a test 2'
+			}
+		}
+	];
+</script>
