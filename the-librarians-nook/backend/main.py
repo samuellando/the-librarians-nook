e3fd8debb1d25a681c10f279c466bbc1d825182e
Samuel Lando <samuel.lando@aol.com>
2023-04-04 11:38:50 -0400

    Add firestore database

--- the-librarians-nook/backend/main.py
+++ the-librarians-nook/backend/main.py
@@ -8,44 +8,35 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+import firebase_admin
+from firebase_admin import firestore
 
+import os
+os.environ['GRPC_DNS_RESOLVER'] = 'native'
+
+firebase_app = firebase_admin.initialize_app()
+db = firestore.client()
 
 app = Flask(__name__)
-
-books = [
-		{
-            "id": "0",
-			"image":
-				'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1170803558l/72193.jpg',
-			"title": 'Harry Potter and the Philosopher’s Stone',
-			"goodReads":
-				'https://www.goodreads.com/book/show/72193.Harry_Potter_and_the_Philosopher_s_Stone?from_search=true&from_srp=true&qid=vHgcnNSuGC&rank=1',
-			"status": 'suggested'
-		},
-		{
-            "id": "1",
-			"image":
-				'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1574199214i/52767659.jpg',
-			"title": 'The End of Everything',
-			"goodReads":
-				'https://www.goodreads.com/book/show/52767659-the-end-of-everything?ref=nav_sb_ss_1_21',
-			"status": 'suggested'
-		}
-	]
 
 @app.route('/books', methods=["GET"])
 @cross_origin()
 def get_books():
-    global books
+    books = []
+    books_ref = db.collection(u'books')
+    docs = books_ref.stream()
+    for doc in docs:
+        book = doc.to_dict()
+        book["id"] = doc.id
+        books.append(book)
+        
     return books;
 
 @app.route('/books', methods=["POST"])
 @cross_origin()
 def post_book():
-    global books
     book = json.loads(request.data)
     url = book["goodReads"]
-    book["id"] = str(len(books))
 
     browser = webdriver.Chrome(ChromeDriverManager().install())
     browser.get(url)
@@ -66,26 +57,29 @@ def post_book():
     book["image"] = image["src"]
     book["status"] = "suggested"
 
-    books.append(book)
+    books_ref = db.collection(u'books')
+    doc = books_ref.add(book)
+
+    book["id"] = doc[1].id
+
     return book;
 
 @app.route('/books/<id>', methods=["PATCH"])
 @cross_origin()
 def patch_book(id):
-    for i, book in enumerate(books):
-        if book["id"] == id:
-            books[i] = json.loads(request.data)
-            return {"id": id, "result": 200};
-    return {"result": 400}
+    books_ref = db.collection(u'books')
+    doc = books_ref.document(id)
+    book = json.loads(request.data)
+    doc.set(book)
+    return {"result": 200}
 
 @app.route('/books/<id>', methods=["DELETE"])
 @cross_origin()
 def remove_book(id):
-    for i, book in enumerate(books):
-        if book["id"] == id:
-            del books[i]
-            return {"id": id, "result": 200};
-    return {"result": 400}
+    books_ref = db.collection(u'books')
+    doc = books_ref.document(id)
+    doc.delete()
+    return {"result": 200}
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
